08f174493aee4c4467e0f01bb176106d7028d25d
Sai Chaithanya A <abbisai7@gmail.com>
2023-08-14 12:34:32 -0700

    episode 1

---
+++ Episode-1/index.js
@@ -0,0 +1,30 @@
+//creating nested html tags
+{/* <div id="parent">
+    <div id="child 1">
+        <h1>H1 tag 1</h1>
+        <h1>H1 tag 2</h1>
+    </div>
+    <div id="child 2">
+        <h1>H1 tag 1</h1>
+        <h1>H1 tag 2</h1>
+    </div>
+</div> */}
+
+// React.createElement(object) => HTML(browser understandable)
+
+const parent = React.createElement("div",{id:"parent"},
+        [React.createElement("div",{id:"child"},
+            [React.createElement("h1",{},"H1 tag 1"),
+            React.createElement("h1",{},"H1 tag 2")]),
+        React.createElement("div",{id:"child"},
+            [React.createElement("h1",{},"H1 tag 1"),
+            React.createElement("h1",{},"H1 tag 2")])
+        ]);
+const heading = React.createElement("h1",{id:"heading"},"Hello World From React");// react object
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);// coverts react object to html for browser to render
+root.render(parent);
+
